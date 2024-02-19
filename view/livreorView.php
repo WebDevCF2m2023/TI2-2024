e74d707342f481a4eeb0512d9b2ae0472111951b
OnlyPassionCode <van.wassenhove.kevin.pro@hotmail.com>
2024-02-19 10:37:14 +0100

    CSS desktop

--- view/livreorView.php
+++ view/livreorView.php
@@ -6,24 +6,24 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>TI2 | Livre d'or</title>
-    <link rel="icon" type="image/x-icon" href="/img/favicon.png">
-    <link rel="stylesheet" href="/css/style.css">
-    <link rel="stylesheet" href="/css/validation.css">
+    <link rel="icon" type="image/x-icon" href="img/favicon.png">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/validation.css">
 </head>
 <body>
     <header>
-        <h1>TI2 | Livre d'or</h1>
+        <h1>Livre d'or</h1>
     </header>
-    <main>
-        <section>
+    <main id="livreor">
+        <section id="formSection">
             <div>
-                <img src="/img/email.png" alt="Image Email">
+                <img src="img/email.png" alt="Image Email" width="300px">
             </div>
             <div>
                 <form action="" method="POST">
                     <h3>Laissez-nous un message</h3>
                     <p id="information-message">Le message a bien été envoyé</p>
-                    <div>
+                    <div id="firstDivForm">
                         <label for="prenom">Prénom *</label>
                         <input type="text" name="prenom" id="prenom">
                     </div>
@@ -39,15 +39,17 @@
                         <label for="message">Message *</label>
                         <textarea name="message" id="message"></textarea>
                     </div>
-                    <p>(*) Ce champ est obligatoire</p>
-                    <input type="submit" value="Envoyer">
+                    <p id="tips">(*) Ce champ est obligatoire</p>
+                    <div id="containerSubmit">
+                        <input type="submit" value="Envoyer">
+                    </div>
                 </form>
             </div>
         </section>
         <section>
             <h2>Message précedents</h2>
             <div class="comment">
-                <p>Lorem a envoyé ce message le 01-02-2023 à 09h12</p>
+                <p><span class="name">Lorem</span> a envoyé ce message le 01-02-2023 à 09h12</p>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus distinctio fugit laborum veritatis in amet ea eum. Illum, exercitationem quam qui repellendus ad nobis, beatae adipisci blanditiis autem facilis assumenda.</p>
             </div>
         </section>
