db812e21746a7222d43b88ad73f9b42eb4884bda
Maskeeded <maskeeded@gmail.com>
2024-02-19 09:26:57 +0100

    Added the load to the livredor page

--- public/index.php
+++ public/index.php
@@ -3,11 +3,19 @@
  * Front Controller de la gestion du livre d'or
  */
 
+
+
+$content = "livreorView"; //Variable pour la page d'accueil
+require_once "../view/$content.php"; // Load the view
+
+
+
+
 /*
  * Chargement des dépendances
  */
 // chargement de configuration
-require_once "../config.php";
+require_once "../config.php"; // load the config with database
 // chargement du modèle de la table livreor
 
 /*
@@ -15,7 +23,7 @@ require_once "../config.php";
  * Avec un try catch pour gérer les erreurs de connexion
  */
 
- 
+
 try {
     $db = new PDO(DB_DRIVER . ":host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=" . DB_CHARSET . ";port=" . DB_PORT, DB_LOGIN, DB_PWD);
 } catch (Exception $e) {
@@ -27,13 +35,13 @@ try {
  * Si le formulaire a été soumis
  */
 
-    // on appelle la fonction d'insertion dans la DB (addLivreOr())
+// on appelle la fonction d'insertion dans la DB (addLivreOr())
 
-    // si l'insertion a réussi
+// si l'insertion a réussi
 
-    // on redirige vers la page actuelle
+// on redirige vers la page actuelle
 
-    // sinon, on affiche un message d'erreur
+// sinon, on affiche un message d'erreur
 
 /*
  * On récupère les messages du livre d'or
@@ -44,5 +52,3 @@ try {
 // fermeture de la connexion
 
 // Appel de la vue
-
-include "../view/livreorView.php";
