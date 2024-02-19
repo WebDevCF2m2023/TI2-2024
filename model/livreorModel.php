1eac7520fcacfb05393e406a3ea6ce06d43bf148
Guillaume <guillaumebaillieux88@gmail.com>
2024-02-19 10:00:08 +0100

    ajinsertLivreDor
    git commit -majinsertLivreDor

--- model/livreorModel.php
+++ model/livreorModel.php
@@ -11,7 +11,7 @@
  */
 function getAllLivreOr(PDO $db): array
 {
-    $sql = "SELECT livreor FROM ti2web2024 ";
+    $sql = "SELECT livreor FROM ti2web2024";
     $query = $db->query($sql);
     $result = $query->fetchAll(PDO::FETCH_ASSOC);
     $query->closeCursor();
@@ -27,12 +27,27 @@ function getAllLivreOr(PDO $db): array
  * @return bool|string
  * Fonction qui insère un message dans la base de données 'ti2web2024' et sa table 'livreor'
  */
-function addLivreOr(PDO $db,
-                    string $firstname,
-                    string $lastname,
-                    string $usermail,
-                    string $message
-                    ): bool|string
+function addLivreOr(PDO $db, string $firstname, string $lastname, string $usermail, string $message): bool|string
 {
+    $firstname = htmlspecialchars(strip_tags(trim($firstname)), ENT_QUOTES);
+    $lastname = htmlspecialchars(strip_tags(trim($lastname)), ENT_QUOTES);
+    $usermail = filter_var($usermail, FILTER_VALIDATE_EMAIL);  // false si le courriel n'est pas valide, sinon on le garde
+    $themessage = htmlspecialchars(strip_tags($message), ENT_QUOTES);
+    
+     
+     if (empty($firstname) || empty($lastname) || $usermail === false || empty($message)) {
+         return false;
+     } // si les données ne sont pas valides, on envoie false
+     
+     $sql = "INSERT INTO livreor (firstname, lastname, ursermail, message) VALUES ('$firstname','$lastname','$usermail', '$message')";
+     try {  // on prépare la requête
+         
+         $db->exec($sql); // on exécute la requête
+         
+         return true; // si tout s'est bien passé, on renvoie true
+     } catch (Exception $e) {
+         
+         return $e->getMessage();  // sinon, on renvoie le message d'erreur
+     }
     return false;
 }
