8c9298b0806089e3fb22ae4c0b41cb68417cc7e3
armaxro <arnaldomrojas@outlook.com>
2024-02-19 12:13:16 +0100

    index

--- public/index.php
+++ public/index.php
@@ -7,18 +7,31 @@
  * Chargement des dépendances
  */
 // chargement de configuration
-require_once "../config.php";
+require_once ("../config.php");
+require_once ("../model/livreorModel.php");
 // chargement du modèle de la table livreor
 
 /*
  * Connexion à la base de données en utilisant PDO
  * Avec un try catch pour gérer les erreurs de connexion
  */
-
+try{
+    $db = new PDO(DB_DRIVER.":host=".DB_HOST.";port=".DB_PORT.";dbname=".DB_NAME.";charset=".DB_CHARSET, DB_LOGIN, DB_PWD);
+}catch(Exception $e){
+    die($e->getMessage());
+}
 /*
  * Si le formulaire a été soumis
  */
-
+if (isset($_GET['usermail'], $_GET['message'])){
+    $insert = addLivreOr($db, $_POST['usermail'], $_POST['message']);
+    if ($insert === true){
+        header("location: ./");
+        exit();
+    }else{
+        $message = $insert;
+    }
+}
     // on appelle la fonction d'insertion dans la DB (addLivreOr())
 
     // si l'insertion a réussi
@@ -32,9 +45,9 @@ require_once "../config.php";
  */
 
 // on appelle la fonction de récupération de la DB (getAllLivreOr())
-
+$livreDor = getAllLivreOr($db);
 // fermeture de la connexion
-
+$db=null;
 // Appel de la vue
 
 include "../view/livreorView.php";
