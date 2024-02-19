d14c8ddcd3bb02d8a1fef69aa05f9640e82fe501
sebastien2506 <jestrovic_8@msn.com>
2024-02-19 15:51:46 +0100

    regler css js

--- public/js/validation.js
+++ public/js/validation.js
@@ -9,11 +9,15 @@ function validateForm(e) {
   if (nom.length > 100 || prenom.length > 100) {
     alert("Le nom ou prénom doit avoir au max 100 caractéres");
     return false;
-  } else if (!/@/.test(mail) || !/\./.test(mail)) {
+  } else if (!/@.*\./.test(mail)) {
     alert("email doit contenir un @ ");
+    return false;
+  }else if (mess.trim().length === 0 || mess.trim().length>600)
+  {
+    alert("faut au moins 1 character et max 600")
     return false;
   }
 
-  alert("Bienvenue " + firstname);
+  alert("Bienvenue " + nom);
   return true;
 }
