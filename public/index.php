b2b69d329ab5b7c6af39ffdbbf24bc6597174274
erdaserhan <erhan209@gmail.com>
2024-02-19 09:43:07 +0100

    Connexion à la base de donnée ok

--- public/index.php
+++ public/index.php
@@ -15,24 +15,36 @@ require_once "../model/livreorModel.php";
  * Avec un try catch pour gérer les erreurs de connexion
  */
 
+ try {
+    $db = new PDO(DB_DRIVER . ":host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=" . DB_CHARSET . ";port=" . DB_PORT, DB_LOGIN, DB_PWD);
+} catch (Exception $e) {
+    die($e->getMessage());
+}
+
 /*
  * Si le formulaire a été soumis
  */
+if (isset($_POST['firstname'], $_POST['lastname'], $_POST['usermail'], $_POST['message'])) {
 
     // on appelle la fonction d'insertion dans la DB (addLivreOr())
-
+    $insert = addLivreOr($db, $_POST['firstname'], $_POST['lastname'], $_POST['usermail'], $_POST['message']);
     // si l'insertion a réussi
-
+    if ($insert) {
     // on redirige vers la page actuelle
-
+    header("Location: ./");
+    exit();
     // sinon, on affiche un message d'erreur
+    } else {
+        $message = "Erreur lors de l'insertion";
+        }
+}
 
 /*
  * On récupère les messages du livre d'or
  */
 
 // on appelle la fonction de récupération de la DB (getAllLivreOr())
-
+$informations = getAllLivreOr($db);
 // fermeture de la connexion
 
 // Appel de la vue
