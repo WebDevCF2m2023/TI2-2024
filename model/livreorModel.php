2477d026a86ba2f8cd0c2bc056de46d4d497ac5a
Guillaume <guillaumebaillieux88@gmail.com>
2024-02-19 10:08:32 +0100

    majrequOnceTryCatch

--- model/livreorModel.php
+++ model/livreorModel.php
@@ -15,7 +15,7 @@ function getAllLivreOr(PDO $db): array
     $query = $db->query($sql);
     $result = $query->fetchAll(PDO::FETCH_ASSOC);
     $query->closeCursor();
-    return [];
+    return []; // retourne un tableau
 }
 
 /**
--- public/index.php
+++ public/index.php
@@ -10,10 +10,27 @@
 require_once "../config.php";
 // chargement du modèle de la table livreor
 
+require_once "../model/livreorModel.php"
+
 /*
  * Connexion à la base de données en utilisant PDO
  * Avec un try catch pour gérer les erreurs de connexion
  */
+
+ try{
+    $MyPDO = new PDO(DB_DRIVER.":host=".DB_HOST.";dbname=".DB_NAME.";port=".DB_PORT.";charset=".DB_CHARSET, DB_LOGIN, DB_PWD);
+
+}catch(Exception $e){
+    die($e->getMessage());
+}
+
+const DB_DRIVER = "mysql";
+const DB_HOST = "localhost";
+const DB_LOGIN = "root";
+const DB_PWD = "";
+const DB_NAME = "ti2web2024";
+const DB_PORT = 3307;
+const DB_CHARSET = "utf8mb4";
 
 /*
  * Si le formulaire a été soumis
