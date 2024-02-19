c2f84423a86adb8459aded362770b19565933445
Helioz7 <baris.aktas@cf2000.onmicrosoft.com>
2024-02-19 16:22:43 +0100

    ti2

--- public/js/validation.js
+++ public/js/validation.js
@@ -18,11 +18,18 @@ function verif(){
 
     
     var messageAlerte = "Bienvenue " + prenom + " " + nom + " !";
+   
     if (prenom.length <= 100 && nom.length <= 100 && message.length <= 600 && /[@]/.test(email)&& /[.]/.test(email)) {
         alert(messageAlerte);
         
         return true;
+    } else {
+        resultDiv.textContent = "Veuillez remplir tous les critères.";
+        resultDiv.style.textAlign = "center";
+        resultDiv.style.color = "red";
+        return false;
     }
+}
 
 
      let check = true;
