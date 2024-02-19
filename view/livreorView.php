6b0a96f2dfe118240472b8a89dafd9edfbc0bd24
Helioz7 <baris.aktas@cf2000.onmicrosoft.com>
2024-02-19 13:23:42 +0100

    ti2

--- view/livreorView.php
+++ view/livreorView.php
@@ -19,32 +19,33 @@
         <?php if(isset($success)): ?>
         <p><?=$success ? "Votre message a bien été enregistré" : "Une erreur s'est produite" ?></p>
         <?php endif; ?>
+
         <form action="" method="POST" onsubmit="return verif()">
-            <p id="error-password" class="error">* Le password doit être/avoir 8 de long / une lettre majuscule / 1 caractère spécial et un chiffre</p>
+            <p id="error-password" class="error">
             <div>
                 <label for="password">Password : </label>
                 <input type="password" name="thepassword" id="password" required>
             </div>
-            <p id="error-nom" class="error">* Le nom doit faire au moins 5 caractère</p>
+            <p id="error-nom" class="error"></p>
             <div>
                 <label for="nom">Nom : </label>
                 <input type="text" name="thenom" id="nom" required>
             </div>
-            <p id="error-email" class="error">* L'email doit avoir un @ et un .</p>
+            <p id="error-email" class="error"></p>
             <div>
                 <label for="email">Votre email :</label>
                 <input type="email" name="themail" id="email" required>
             </div>
-            <p id="error-themessage" class="error">* Le message ne doit pas être vide</p>
+            <p id="error-themessage" class="error"></p>
             <textarea name="themessage" id="themessage" maxlength="2050" required></textarea>
             <div>
                 <input type="submit" value="Envoyer">
             </div>
         </form>
-
-        <section id="informations">
 </body>
 </html>
     <script src="js/livredor.js"></script>
+
+    
 </body>
 </html>
