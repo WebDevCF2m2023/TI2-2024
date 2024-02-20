38e89ec6eaf11ad7dcaed7bd97bcf6baebcfa3c0
Maskeeded <maskeeded@gmail.com>
2024-02-20 11:30:46 +0100

    Update CSS styles and form validation
    Changed the title of comments to be more responsive
    Changed the form and submit container, to adjust to height change
    changed max button width to have a min/max

--- view/livreorView.php
+++ view/livreorView.php
@@ -9,7 +9,6 @@
     <title>TI2 | Livre d'or</title>
     <link rel="icon" type="image/x-icon" href="img/favicon.png">
     <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/validation.css">
 </head>
 
 <body>
@@ -19,44 +18,43 @@
         <div class="main-container">
             <img src="img/email.png" class="image-mail" alt="Image of a woman pointing at a mail">
 
-
             <div class="form-container">
                 <!-- Your form fields here -->
 
                 <h2 id="top-form-message">Laissez-nous un message</h2>
 
-                <form action="" id="form" method="POST">
+                <form action="index.php" id="form" method="POST">
 
                     <div class="input-group">
-                        <label for="name">Prenom *</label>
-                        <input type="text" name="firstname" id="form-prenom" placeholder="Prenom">
+                        <label for="form-prenom">Prenom *</label>
+                        <input type="text" name="firstname" required id="form-prenom" placeholder="Prenom">
                     </div>
 
                     <div class="input-group">
-                        <label for="name">Nom *</label>
-                        <input type="text" name="lastname" id="form-nom" placeholder="Nom">
+                        <label for="form-nom">Nom *</label>
+                        <input type="text" name="lastname" required id="form-nom" placeholder="Nom">
                     </div>
 
                     <div class="input-group">
-                        <label for="name">E-mail *</label>
-                        <input type="text" name="usermail" id="form-mail" placeholder="Mail">
+                        <label for="form-mail">E-mail *</label>
+                        <input type="text" name="usermail" required id="form-mail" placeholder="Mail">
                     </div>
 
                     <div class="input-group">
-                        <label for="message">Message *</label>
-                        <textarea name="message" id="form-message" required placeholder="Write your message"></textarea>
+                        <label for="form-message">Message *</label>
+                        <textarea name="message" id="form-message" required
+                            placeholder="Ecrivez votre message"></textarea>
                     </div>
 
                     <h2 id='obligatoire'>(*) Ce champ est obligatoire</h2>
-                    <h2 id='helper'>(*) Ce champ est obligatoire</h2>
+                    <h2 id='helper'>(*) Helper Text</h2>
 
                     <div class="submit-container">
                         <input type="submit" id="submit-form" value="Envoyer">
                     </div>
 
-
+                </form>
             </div>
-            </form>
 
         </div>
 
