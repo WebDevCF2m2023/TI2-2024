9916508548a66fcc005a0244991b893a86594491
OnlyPassionCode <van.wassenhove.kevin.pro@hotmail.com>
2024-02-25 14:39:42 +0100

    no cache

--- view/livreorView.php
+++ view/livreorView.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+    <meta http-equiv="Expires" content="0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>TI2 | Livre d'or</title>
     <link rel="icon" type="image/x-icon" href="img/favicon.png">
